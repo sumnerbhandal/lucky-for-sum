7ae290610a1f78de94ada260b5a5799ea15c8eb2
Sumner Bhandalgit config --global user.email sumnerbhandal@live.com <sumnerbhandal@live.com>
2020-07-27 22:33:25 +0100

    build test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Route, Link, Switch, Redirect, useHistory } fr
 export default function App() {
   const name = 'John Doe';
   return (
-   <Router basename={process.env.PUBLIC_URL}>
+   <Router>
     <main>
       <nav>
         <ul>
