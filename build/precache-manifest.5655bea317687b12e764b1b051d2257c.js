e0ecfbdd1229c6b952fbf4f5e16d32a7fba08f2d
Sumner Bhandalgit config --global user.email sumnerbhandal@live.com <sumnerbhandal@live.com>
2020-07-28 15:35:40 +0100

    strapi api config amend

---
+++ build/precache-manifest.5655bea317687b12e764b1b051d2257c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8154e570cafcb626463383090dd6d1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f835bcfa8422809fdb5",
+    "url": "/static/css/main.d4ba97a2.chunk.css"
+  },
+  {
+    "revision": "f08bbb389ba8d4b6c14a",
+    "url": "/static/js/2.709ea1bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.709ea1bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f835bcfa8422809fdb5",
+    "url": "/static/js/main.791067e5.chunk.js"
+  },
+  {
+    "revision": "aebf0817544c204b4a0e",
+    "url": "/static/js/runtime-main.17026185.js"
+  }
+]);
