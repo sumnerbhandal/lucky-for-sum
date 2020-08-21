12653e3e53e4fae66b69b282e982b646c30e2fa9
Sumner Bhandalgit config --global user.email sumnerbhandal@live.com <sumnerbhandal@live.com>
2020-08-21 12:48:05 +0100

    invalid select value

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -20,7 +20,7 @@ export default function CountactForm() {
 
   const formValidation = (event) => {
     setErrorMessages([]);
-    
+
     const isNameValid = name !== "";
     const isMessageValid = message !== "";
     const isSubjectValid = subject !== "";
@@ -77,7 +77,7 @@ export default function CountactForm() {
                 autocomplete="name"
                 placeholder="Name"
                 name="Name"
-                onChange={e => setName(e.target.value)}
+                onChange={e => setName(e.target.innerHTML)}
                 />
             </div>
             <div className="contact-email-container">
@@ -99,11 +99,11 @@ export default function CountactForm() {
                 placeholder="Help with my order"
                 onChange={e => setSubject(e.target.value)}
                 >
-                <option value="Just Saying Hello">Just Saying Hello</option>
-                <option value="Delivery Query">Delivery Query</option>
-                <option value="Returns &amp; Refunds">Returns &amp; Refunds</option>
-                <option value="Problem With A Product">Problem With A Product</option>
-                <option value="Other">Other</option>
+                <option value="1">Just Saying Hello</option>
+                <option value="2">Delivery Query</option>
+                <option value="3">Returns &amp; Refunds</option>
+                <option value="4">Problem With A Product</option>
+                <option value="5">Other</option>
               </select>
             </div>
             <div className="message-container">
