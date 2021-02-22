162b1017a06b37cdb83d44ebf5651d89b6254bc3
Sumner Bhandalgit config --global user.email sumnerbhandal@live.com <sumnerbhandal@live.com>
2021-02-22 22:32:10 +0000

    pushed content up the page

--- src/home/home.js
+++ src/home/home.js
@@ -64,7 +64,7 @@ const HomePage = (props) => {
             {homePageProjects.map((item, index) => (
                 <div className="project-preview-container hp-section" id="project-container" key={index}>
                     <div className={`project-preview ${inView ? "before" : ""}`}>
-                    <h2>
+                        <h2>
                             {item.title}
                         </h2>
                         <Link to={`/project/${item.url}-pid-${item.id}`} className="project-preview-thumbnail" id={item.id} title={item.url} onClick={projectImageOpen}  tabIndex="0">
