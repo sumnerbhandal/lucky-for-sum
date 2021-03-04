4684334f2058b40ea108f3dff466580ebeedb387
Sumner Bhandalgit config --global user.email sumnerbhandal@live.com <sumnerbhandal@live.com>
2021-03-04 16:55:03 +0000

    addition of service worker back in

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.register();
+serviceWorker.register();
 
 
-import React from 'react';
-import App from './App';
-import { hydrate, render } from "react-dom";
-import './index.css';
+// import React from 'react';
+// import App from './App';
+// import { hydrate, render } from "react-dom";
+// import './index.css';
  
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+// const rootElement = document.getElementById("root");
+// if (rootElement.hasChildNodes()) {
+//   hydrate(<App />, rootElement);
+// } else {
+//   render(<App />, rootElement);
+// }
