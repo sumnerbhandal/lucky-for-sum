1b6cb2784a4d8ef2e3cb7c7f458dff5196690888
Sumner Bhandalgit config --global user.email sumnerbhandal@live.com <sumnerbhandal@live.com>
2022-01-06 14:51:26 +0000

    rewritten the about me section

--- src/home/components/home---about-me.js
+++ src/home/components/home---about-me.js
@@ -10,9 +10,9 @@ export const About = () => {
         <div className="aboutMe">
             <div className="column">
                 <h1 aria-level="1" tabIndex="0">Hi, my name's Sumner Bhandal <span role="img" aria-label="Welcome Wave">👋🏽</span></h1>
-                <p tabIndex="0">I'm a Senior User Interface Designer at one of the leading British retailers, <a href="https://www.next.co.uk/" rel="noopener noreferrer" target="_blank">Next</a>. I design experiences, interactions and implementation strategies that look to take the brilliant quantitative and qualitative research done by the business and see them through to fruition. </p>
-                <p tabIndex="0">Having plied my trade as a graphic designer, I have had the opportunity to expand my repertoire to adopt human-centred research methods through studying Human Computer Interaction, and marry that with a working knowledge of <Link to="/project/portfolio-for-all-pid-1">inclusive design</Link> to try and make the internet a bit more accessible.</p>
-                <p tabIndex="0">My current challenge is utilising all of that knowledge and leveraging my understanding of front-end development to help shape the future of the Next Platform Services and create a responsive, inclusive and versatile interface that will provide the foundation of the website for years to come.</p>
+                <p tabIndex="0">I'm a product specialist who designs experiences, interactions and implementation strategies that look to solve the everyday problems that we have learned to work around.</p>
+                <p tabIndex="0">Having started my career in visual communication, I have had the opportunity to expand my repertoire to adopt human-centred research methods through studying Human Computer Interaction and working alongside some brilliant user researchers. I continue to use that as a basis to inform my working knowledge of <Link to="/project/portfolio-for-all-pid-1">inclusive design</Link> to try and make the digital age a bit more accessible.</p>
+                <p tabIndex="0">My current challenge is to help make contracts simpler for all at <a href="https://www.robinai.co.uk/" rel="noopener noreferrer" target="_blank">Robin AI</a>.</p>
                 </div>
             <div className="column">
                  <LazyLoad offset={400} throttle={50}>
