689935fbea1c9c3be35cebe22f2ab38739907d1c
Sumner Bhandalgit config --global user.email sumnerbhandal@live.com <sumnerbhandal@live.com>
2022-03-21 23:23:28 +0000

    updated bio

--- src/home/components/home---about-me.js
+++ src/home/components/home---about-me.js
@@ -11,7 +11,7 @@ export const About = () => {
             <div className="column">
                 <h1 aria-level="1" tabIndex="0">Hi, my name's Sumner Bhandal <span role="img" aria-label="Welcome Wave">👋🏽</span></h1>
                 <p tabIndex="0">I'm a product specialist who designs experiences, interactions and implementation strategies that look to solve the everyday problems that we have learned to work around.</p>
-                <p tabIndex="0">Having started my career in visual communication, I have had the opportunity to expand my repertoire to adopt human-centred research methods through studying Human Computer Interaction and working alongside some brilliant user researchers. I continue to use that as a basis to inform my working knowledge of <Link to="/project/portfolio-for-all-pid-1">inclusive design</Link> to try and make the digital age a bit more accessible.</p>
+                <p>Previously working to keep students safe online and offline at Education Technology company Impero Software. More recently helping to scale a leading e-commerce retailer and build better processes at <a href="https://next.co.uk/" rel="noopener noreferrer" target="_blank">Next</a></p>
                 <p tabIndex="0">My current challenge is to help make contracts simpler for all at <a href="https://www.robinai.co.uk/" rel="noopener noreferrer" target="_blank">Robin AI</a>.</p>
                 </div>
             <div className="column">
